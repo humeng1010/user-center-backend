d77b976e522e96b08e5c0612f3be9bdce83bf8b6
humeng <humeng_mail@163.com>
2023-06-18 12:14:03 +0800

    update: 新增后端登陆拦截器,未登陆不能访问具体接口

--- src/main/java/com/usercenter/config/WebMvcConfig.java
+++ src/main/java/com/usercenter/config/WebMvcConfig.java
@@ -1,10 +1,27 @@
 package com.usercenter.config;
 
+import com.usercenter.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Resource
+    private LoginInterceptor loginInterceptor;
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                // 拦截所有请求
+                .addPathPatterns("/**")
+                // 放行登陆和注册请求
+                .excludePathPatterns(
+                        "/user/register",
+                        "/user/login");
+
+    }
 }
