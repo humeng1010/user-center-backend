84d451df0066688f9d03929c5cf1aae9111c8101
humeng <humeng_mail@163.com>
2023-06-21 18:09:26 +0800

    update: 新增tag数据库表

--- src/main/resources/db/user.sql
+++ src/main/resources/db/user.sql
@@ -19,3 +19,22 @@ create table user
 )
     comment '用户表';
 
+
+-- auto-generated definition
+create table tag
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    tagName    varchar(256)                       null comment '标签名称',
+    userId     bigint                             null comment '用户id',
+    parentId   bigint                             null comment '父标签id',
+    isParent   tinyint                            not null comment '是否是父标签,0不是,1是',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete   tinyint  default 0                 not null comment '是否删除,0未删除,1删除',
+    constraint uniIdx_tagName
+        unique (tagName)
+)
+    comment '标签';
+
+
